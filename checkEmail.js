d7d921075362d3bfc3f9b05f54cd3deb5c77c4e9
Hugo Tsao <32349120+dongdong7048@users.noreply.github.com>
2022-08-04 15:15:44 +0800

    加入中文註解

--- checkEmail.js
+++ checkEmail.js
@@ -1,7 +1,7 @@
 /** 檢核email輸入是否合法 **/
 function checkEmail() {
       let req = /^([a-zA-Z0-9]+[_|\_|\.|\-]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.|\-]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
-      const input_email = $('.input-email'); //use jquery
+      const input_email = $('.input-email'); // <input type="text" class="input-pwd" /> , use jquery
       if (!req.test(input_email.val()))
         alert("this email is inValid!")
       else
